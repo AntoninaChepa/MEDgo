c5bbcac588590a8b54cee936f437af932a8de6d1
karol <kpasieczny@fbk.eu>
2024-11-08 22:29:49 +0100

    -finished transport data integration

--- src/main/java/com/hack/demo/StartupConfig.java
+++ src/main/java/com/hack/demo/StartupConfig.java
@@ -1,6 +1,8 @@
 package com.hack.demo;
 
+import com.hack.demo.data.TransportDao;
 import com.hack.demo.data.VehicleStatsDao;
+import com.hack.demo.domain.Transport;
 import com.hack.demo.domain.VehicleStats;
 import com.hack.demo.integration.CsvParserService;
 import org.springframework.boot.CommandLineRunner;
@@ -16,9 +18,12 @@ public class StartupConfig {
 
     private final VehicleStatsDao vehicleStatsDao;
 
-    public StartupConfig(CsvParserService csvParserService, VehicleStatsDao vehicleStatsDao) {
+    private final TransportDao transportDao;
+
+    public StartupConfig(CsvParserService csvParserService, VehicleStatsDao vehicleStatsDao, TransportDao transportDao) {
         this.csvParserService = csvParserService;
         this.vehicleStatsDao = vehicleStatsDao;
+        this.transportDao = transportDao;
     }
 
     @Bean
@@ -28,6 +33,11 @@ public class StartupConfig {
             List<VehicleStats> vehicleStatsList = csvParserService.loadVehicleStatsFromCsv();
             vehicleStatsList.forEach(System.out::println); // Print each record for demonstration
             vehicleStatsDao.batchInsertVehicleStats(vehicleStatsList);
+
+            transportDao.deleteAll();
+            List<Transport> transportList = csvParserService.loadTransportData();
+            transportList.forEach(System.out::println); // Print each record for demonstration
+            transportDao.batchInsert(transportList);
         };
     }
 }
--- src/main/java/com/hack/demo/data/TransportMapper.java
+++ src/main/java/com/hack/demo/data/TransportMapper.java
@@ -9,26 +9,38 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+
 public class TransportMapper implements RowMapper<Transport> {
     public static final String SELECT_ALL = "SELECT * FROM transport";
-    public static final String SELECT_BY_TRANSPORT_NUMMER = "SELECT * FROM transport WHERE transport_nummer = ?";
+    public static final String SELECT_BY_TRANSPORT_NUMBER = "SELECT * FROM transport WHERE transport_number = ?";
+    public static final String DELETE_ALL = "DELETE FROM transport";
+    public static final String INSERT = """
+        INSERT INTO transport (
+            transport_number, transport_date, transport_start, transport_end, from_location, 
+            from_street, to_location, to_street, transport_type, reference_number, 
+            total_km, fleet_class, section_location
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """;
 
     @Override
     public Transport mapRow(ResultSet rs, int rowNum) throws SQLException {
         Transport transport = new Transport();
-        transport.setTransportnummer(rs.getString("transport_nummer"));
-        transport.setTransportdatum(rs.getObject("transport_datum", LocalDate.class));
-        transport.setTranhstart(rs.getObject("tranh_start", LocalTime.class));
-        transport.setTranhende(rs.getObject("tranh_ende", LocalTime.class));
-        transport.setTranvonort(rs.getString("tran_von_ort"));
-        transport.setTranvonstrasse(rs.getString("tran_von_strasse"));
-        transport.setTranbisort(rs.getString("tran_bis_ort"));
-        transport.setTranbisstrasse(rs.getString("tran_bis_strasse"));
-        transport.setTransportart(rs.getString("transport_art"));
-        transport.setBezugnr(rs.getString("bezug_nr"));
-        transport.setKmtotale(rs.getInt("km_totale"));
-        transport.setFuhrparkklasse(rs.getString("fuhrpark_klasse"));
-        transport.setSektionort(rs.getString("sektion_ort"));
+
+        // Mapping snake_case column names to camelCase entity fields
+        transport.setTransportNumber(rs.getString("transport_number"));
+        transport.setTransportDate(rs.getObject("transport_date", LocalDate.class));
+        transport.setTransportStart(rs.getObject("transport_start", LocalTime.class));
+        transport.setTransportEnd(rs.getObject("transport_end", LocalTime.class));
+        transport.setFromLocation(rs.getString("from_location"));
+        transport.setFromStreet(rs.getString("from_street"));
+        transport.setToLocation(rs.getString("to_location"));
+        transport.setToStreet(rs.getString("to_street"));
+        transport.setTransportType(rs.getString("transport_type"));
+        transport.setReferenceNumber(rs.getString("reference_number"));
+        transport.setTotalKm(rs.getInt("total_km"));
+        transport.setFleetClass(rs.getString("fleet_class"));
+        transport.setSectionLocation(rs.getString("section_location"));
+
         return transport;
     }
 }
