e7029e5956082fcd8f96605c4a52f788d82bc5dd
francesco <francesco.santini@wuerth.it>
2024-11-09 01:09:03 +0100

    added shadcn datatable stuff

---
+++ frontend/medgo-web/src/pages/dashboard/rides.tsx
@@ -0,0 +1,10 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import Layout from "@/modules/layout/layout";
+import React from "react";
+import { SidebarProvider } from "@/components/ui/sidebar";
+
+const RidesPage = () => {
+	return <Layout></Layout>;
+};
+
+export default RidesPage;
