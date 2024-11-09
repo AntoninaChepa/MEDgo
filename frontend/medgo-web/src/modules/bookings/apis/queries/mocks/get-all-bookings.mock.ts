6d624174315adb03bcea95f247d2c15b7013edff
Gabriele Concli <gabriele@plannix.co>
2024-11-09 01:24:24 +0100

    mock update

--- frontend/medgo-web/src/modules/bookings/apis/queries/mocks/get-all-bookings.mock.ts
+++ frontend/medgo-web/src/modules/bookings/apis/queries/mocks/get-all-bookings.mock.ts
@@ -4,41 +4,60 @@ import { BookingsOutput } from "../get-all-bookings";
 const mockedBookings = [
   {
     user: {
-      first_name: "",
-      last_name: "",
-      email: "",
-      phone: "",
+      first_name: "John",
+      last_name: "Doe",
+      email: "john@doe.com",
+      phone: "+1234567890",
     },
     departure: {
-      city: "",
-      street: "",
+      city: "Meran",
+      street: "Piazza Walther",
     },
     arrival: {
-      city: "",
-      street: "",
+      city: "Bozen",
+      street: "Piazza Walther",
     },
-    seat_type: "",
-    arrival_time: "",
-    pickup_time: "",
+    seat_type: "idk",
+    arrival_time: "2024-11-09T00:23:23.901Z",
+    pickup_time: "2024-11-09T00:23:23.901Z",
   },
   {
     user: {
-      first_name: "",
-      last_name: "",
-      email: "",
-      phone: "",
+      first_name: "John",
+      last_name: "Doe",
+      email: "john@doe.com",
+      phone: "+1234567890",
     },
     departure: {
-      city: "",
-      street: "",
+      city: "Meran",
+      street: "Piazza Walther",
     },
     arrival: {
-      city: "",
-      street: "",
+      city: "Bozen",
+      street: "Piazza Walther",
     },
-    seat_type: "",
-    arrival_time: "",
-    pickup_time: "",
+    seat_type: "idk",
+    arrival_time: "2024-11-09T00:23:23.901Z",
+    pickup_time: "2024-11-09T00:23:23.901Z",
+  },
+  {
+    user: {
+      first_name: "John",
+      last_name: "Doe",
+      email: "john@doe.com",
+      phone: "+1234567890",
+    },
+    departure: {
+      city: "Meran",
+      street: "Piazza Walther",
+    },
+    arrival: {
+      city: "Bozen",
+      street: "Piazza Walther",
+    },
+    seat_type: "idk",
+    arrival_time: "2024-11-09T00:23:23.901Z",
+    pickup_time: "2024-11-09T00:23:23.901Z",
   },
 ] satisfies Booking[];
 
